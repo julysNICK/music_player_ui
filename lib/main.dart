4f0308b920aa8f3b1fbebe7dafb298aa37b1e34c
julysNICK <julystube@gmail.com>
2023-02-17 18:37:20 -0300

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -171,7 +171,10 @@ class _MyPlayerMusicState extends State<MyPlayerMusic> {
   GestureDetector CardRecentlySong() {
     return GestureDetector(
       onTap: () {
-        print('object');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MyPlayer()),
+        );
       },
       child: Container(
         child: Column(
@@ -217,73 +220,75 @@ class _MyPlayerMusicState extends State<MyPlayerMusic> {
           scrollDirection: Axis.horizontal,
           physics: const BouncingScrollPhysics(),
           itemCount: 3,
+          itemExtent: 400,
           itemBuilder: (ctx, index) {
             return Container(
-              margin: EdgeInsets.only(left: index != 0 ? 20 : 10),
-              child: Stack(
-                children: [
-                  Image.asset(
-                    'assets/image1.png',
-                    fit: BoxFit.cover,
-                    width: 390,
-                  ),
-                  Container(
-                    width: 390,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.8),
-                        ],
-                        stops: const [0.0, 1.0],
-                      ),
+              child: Center(
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/image1.png',
+                      fit: BoxFit.cover,
+                      width: 390,
                     ),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'FEATURED PLAYLIST',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                    Container(
+                      width: 390,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.8),
+                          ],
+                          stops: const [0.0, 1.0],
+                        ),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'FEATURED PLAYLIST',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                'Follow The Leader ft. Jennifer ',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Follow The Leader ft. Jennifer ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                'Wisin & Yandel | Featured Song | 11.12.2021',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 15,
+                                Text(
+                                  'Wisin & Yandel | Featured Song | 11.12.2021',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             );
           }),
@@ -315,6 +320,98 @@ class _MyPlayerMusicState extends State<MyPlayerMusic> {
             borderRadius: BorderRadius.all(
               Radius.circular(30),
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyPlayer extends StatefulWidget {
+  const MyPlayer({super.key});
+
+  @override
+  State<MyPlayer> createState() => _MyPlayerState();
+}
+
+class _MyPlayerState extends State<MyPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff1d1f3e),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image1.png'),
+            fit: BoxFit.cover,
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff111536), Color(0x00111536), Color(0xff111536)],
+            stops: [0.0, 0.5, 1.0],
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.share_rounded,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.3,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffFF7A8A),
+                        image: DecorationImage(
+                          image: AssetImage('assets/2.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.music_note,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
